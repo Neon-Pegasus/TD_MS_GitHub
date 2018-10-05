d4ed12e135606637ba0d266ab603498ea545cf6e
JBen13 <jessicabenjaminxiii@gmail.com>
2018-10-04 20:26:16 -0700

    Adds get request to Github for User's Repos.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const app = express();
+const axios = require('axios');
 const bodyParser = require('body-parser');
+const octokit = require('@octokit/rest')({debug:true})
+// const db = require('../database/database.js');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
+const gitServer = express();
 const port = 4200; 
 
-app.listen(port, function() {
-  console.log('Server is running on port: '+ port);
+gitServer.use(bodyParser.json());
+// gitServer.use(bodyParser.urlencoded({extended:true}));
+
+gitServer.listen(port, function() {
+  console.log(`listening on ${port}`);
 });
+
+//GET: User's repos from GitHub
+gitServer.get('/user/repos', (req, res) => {
+  var username = 'therobinkim';
+
+  var api = `https://api.github.com/users/${username}/repos`
+  axios.get(api, {headers: {Authorization: `Bearer${process.env.GITHUB_TOKEN}`}} )
+  .then((data) => {
+    console.log(data);
+  })
+})
+
+
+
+
+
