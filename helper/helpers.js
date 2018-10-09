89e98589731e0453a488bffbcb7f1461e4f0eb90
JBen13 <jessicabenjaminxiii@gmail.com>
2018-10-09 15:29:11 -0700

    Updates code and cleans up the linter errors. #

--- helper/helpers.js
+++ helper/helpers.js
@@ -1,108 +1,107 @@
 const request = require('request-promise');
 require('dotenv').config();
 
-
+const ghToken = 'b0200925c2c1315b86b665b308fa0de67f4d478a';
 
 // List User's Repos from Github
 const getReposByUser = (username, callback) => {
-  let options = {
+  const options = {
     method: 'GET',
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `bearer ${ghToken}`
-    }
-  }
-  request.get(options, function(err, res) {
+      Authorization: `bearer ${ghToken}`,
+    },
+  };
+  request.get(options, (err, res) => {
     if (err) {
       console.log('Error - request to api failed');
     } else {
-      console.log('Success- retrieved Repos');
+      console.log('Success- retrieved user repos');
       callback(res.body);
     }
-  })
+  });
 };
 
-// List the User's repo's pull request review comments 
+// List the User's repo's pull request review comments
 const listCommentsInARepo = (username, repo, callback) => {
-  let options = {
+  const options = {
     method: 'GET',
     url: `https://api.github.com/repos/${username}/${repo}/issues/comments`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `bearer ${ghToken}`
-    }
-  }
-  request.get(options, function(err, res) {
+      Authorization: `bearer ${ghToken}`,
+    },
+  };
+  request.get(options, (err, res) => {
     if (err) {
       console.log('Error - request to api failed');
     } else {
       console.log('Success- retrieved Repos comments');
       callback(res.body);
     }
-  })
+  });
 };
 
 // List Github Random Organizations
 const listOrganizations = (callback) => {
-  let options = {
+  const options = {
     method: 'GET',
-    url: `https://api.github.com/organizations`,
+    url: 'https://api.github.com/organizations',
     headers: {
       'User-Agent': 'request',
-      'Authorization': `bearer ${ghToken}`
-    }
-  }
-  request.get(options, function(err, res) {
+      Authorization: `bearer ${ghToken}`,
+    },
+  };
+  request.get(options, (err, res) => {
     if (err) {
       console.log('Error - request to api failed');
     } else {
       console.log('Success- retrieved Repos');
       callback(res.body);
     }
-  })
+  });
 };
-
 
 // Searches repositories by Stargazers over 10,000 and language: javascript
 const reposByStars = (callback) => {
-  let options = {
+  const options = {
     method: 'GET',
     url: 'https://api.github.com/search/repositories?q=stars:>10000+language:javascript',
     headers: {
       'User-Agent': 'request',
-      'Authorization': `bearer ${ghToken}`
-    }
-  }
-  request.get(options, function(err, res) {
+      Authorization: `bearer ${ghToken}`,
+    },
+  };
+  request.get(options, (err, res) => {
     if (err) {
       console.log('Error - request to api failed');
     } else {
       console.log('Success- retrieved Repos');
       callback(res.body);
     }
-  })
+  });
 };
 
 // List the Top Repos pull request review comments
 const listComments = (url, callback) => {
-  let options = {
+  const options = {
     method: 'GET',
     url: `${url}/issues/comments`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `bearer ${ghToken}`
-    }
-  }
+      Authorization: `bearer ${ghToken}`,
+    },
+  };
   console.log(options.url);
-  request.get(options, function(err, res) {
+  request.get(options, (err, res) => {
     if (err) {
       console.log('Error - request to api failed');
     } else {
       console.log('Success - retrieved Repos comments');
       callback(res.body);
     }
-  })
+  });
 };
 
 module.exports.getReposByUser = getReposByUser;
@@ -112,5 +111,5 @@ module.exports.reposByStars = reposByStars;
 module.exports.listComments = listComments;
 
 /**
- * These helper functions are used to query data from the Github API. 
+ * These helper functions are used to query data from the Github API.
  */
