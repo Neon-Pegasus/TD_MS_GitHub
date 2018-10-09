fc0bf20817c54b62947e4b7c0a3072aeb4468788
JBen13 <jessicabenjaminxiii@gmail.com>
2018-10-09 14:35:19 -0700

    Adds helper for listComments for top repos

--- helper/helpers.js
+++ helper/helpers.js
@@ -1,8 +1,19 @@
 const request = require('request-promise');
 require('dotenv').config();
 
+var ghToken = '';
 
-const request = () => {
+
+// User's Repos from Github
+const getReposByUser = (username, callback) => {
+  let options = {
+    method: 'GET',
+    url: `https://api.github.com/users/${username}/repos`,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `bearer ${ghToken}`
+    }
+  }
   request.get(options, function(err, res) {
     if (err) {
       console.log('Error - request to api failed');
@@ -13,30 +24,24 @@ const request = () => {
   })
 };
 
-// User's Repos from Github
-const getReposByUser = (username, callback) => {
-  let options = {
-    method: 'GET',
-    url: `https://api.github.com/users/${username}/repos`,
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': `bearer ${process.env.GITHUB_TOKEN}`
-    }
-  }
-  return request(options);
-};
-
-//User's Comments/Reviews by Repos
+//User's Comments/Reviews to a pull request by Repos
 const listCommentsInARepo = (username, repo, callback) => {
   let options = {
     method: 'GET',
-    url: `https://api.github.com/repos/${username}/${repo}/pulls/comments`,
+    url: `https://api.github.com/repos/${username}/${repo}/issues/comments`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `bearer ${process.env.GITHUB_TOKEN}`
+      'Authorization': `bearer ${ghToken}`
     }
   }
-  return request(options);
+  request.get(options, function(err, res) {
+    if (err) {
+      console.log('Error - request to api failed');
+    } else {
+      console.log('Success- retrieved Repos comments');
+      callback(res.body);
+    }
+  })
 };
 
 // List Github Organizations
@@ -46,40 +51,67 @@ const listOrganizations = (callback) => {
     url: `https://api.github.com/organizations`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `bearer ${process.env.GITHUB_TOKEN}`
+      'Authorization': `bearer ${ghToken}`
     }
   }
-  return request(options);
+  request.get(options, function(err, res) {
+    if (err) {
+      console.log('Error - request to api failed');
+    } else {
+      console.log('Success- retrieved Repos');
+      callback(res.body);
+    }
+  })
 };
 
-//Organizations members 
-const listOrgsMembers = (org, callback) => {
-  let options = {
-    method: 'GET',
-    url: `https://api.github.com/orgs/${org}/members`,
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': `bearer ${process.env.GITHUB_TOKEN}`
-    }
-  }
-  return request(options);
-};
 
 // Search repositories by stargazers 
 const reposByStars = (callback) => {
   let options = {
     method: 'GET',
-    url: 'https://api.github.com/search/repositories/stars:>10000 fork:true language:javascript',
+    url: 'https://api.github.com/search/repositories?q=stars:>10000+language:javascript',
     headers: {
       'User-Agent': 'request',
-      'Authorization': `bearer ${process.env.GITHUB_TOKEN}`
+      'Authorization': `bearer ${ghToken}`
     }
   }
-  return request(options);
+  request.get(options, function(err, res) {
+    if (err) {
+      console.log('Error - request to api failed');
+    } else {
+      console.log('Success- retrieved Repos');
+      callback(res.body);
+    }
+  })
+};
+
+// List top Repos comments
+const listComments = (url, callback) => {
+  let options = {
+    method: 'GET',
+    url: `${url}/issues/comments`,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `bearer ${ghToken}`
+    }
+  }
+  console.log(options.url);
+  request.get(options, function(err, res) {
+    if (err) {
+      console.log('Error - request to api failed');
+    } else {
+      console.log('Success - retrieved Repos comments');
+      callback(res.body);
+    }
+  })
 };
 
 module.exports.getReposByUser = getReposByUser;
 module.exports.listCommentsInARepo = listCommentsInARepo;
 module.exports.listOrganizations = listOrganizations;
-module.exports.listOrgMembers = listOrgsMembers;
 module.exports.reposByStars = reposByStars;
+module.exports.listComments = listComments;
+
+/**
+ * These helper functions are used to query data from the Github API. 
+ */
