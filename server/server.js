aa861e7f32d216a81ae1fe62ea2a60fbb10c9dc8
JBen13 <jessicabenjaminxiii@gmail.com>
2018-10-09 23:02:10 -0700

    Updates request for data functions and adds post request for organizations.#

--- server/server.js
+++ server/server.js
@@ -2,148 +2,134 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 const api = require('../helper/helpers.js');
-require('dotenv').config()
+require('dotenv').config();
 
 
 const gitServer = express();
-const port = process.env.PORT || 4200; 
+const port = process.env.PORT || 4200;
 
 gitServer.use(bodyParser.json());
 
-gitServer.listen(port, function() {
+gitServer.listen(port, () => {
   console.log(`listening on ${port}`);
 });
 
 
-
-//User's repos from GitHub
-  gitServer.get('/api/user/repos', (req, res) => {
-    let username = req.body.username || 'therobinkim';
-    let userId = 0;
-    api.getReposByUser(username, function(res) {
-      let newData = JSON.parse(res);
-      var userRepos = newData.map(repo => JSON.stringify((repo.name)));
-      console.log(userRepos);
-      db.User.create({
-          userId: userId, 
-          userName: username, 
-          repoNameList: userRepos
-        }).then((user) => {
-          console.log('Sucess - Data is saved', user);
-        }).catch((error) => {
-          console.log('Error - NOT saved', error)
-        });
-    })
-    .catch(error => console.log('Error - Repo comments were NOT saved', error));
-    res.end();
-  });
-
-  // User's repos and User's review comments
-  gitServer.get('/api/user/repo/review', (req, res) => {
-    let username = 'therobinkim';
-    let repo ='lets-recreate-axios';
-    let repoId = 0;
-    api.listCommentsInARepo(username, repo, function(res) {
-      let newRepo = JSON.parse(res);
-      let data = newRepo.map((repo) => {
-        if (repo.author_association !== 'CONTRIBUTOR') {
-          return repo.body;
-        }
-      });
-      console.log(data);
-      db.Repo.create({
-        repoId: repoId,
-        repoName: repo,
-        commentsBody: data, 
-      }).then((data) => {
-        console.log('Repo review comments have been saved!', data);
-      }).catch((error) => {
-        console.log('Error - Repo Review was NOT saved', error);
-      })
-      
-    })
-    .catch(error => console.log('Error - Repo comments were NOT saved', error));
-    res.end();
+// User's repos from GitHub
+gitServer.get('/api/user/repos', (req, res) => {
+  const username = req.body.username || 'therobinkim';
+  const userId = 0;
+  api.getReposByUser(username, (unit) => {
+    const newData = JSON.parse(unit);
+    const userRepos = newData.map(repo => JSON.stringify((repo.name)));
+    console.log(userRepos);
+    db.User.create({
+      userId,
+      userName: username,
+      repoNameList: userRepos,
+    }).then((user) => {
+      console.log('Sucess - Data is saved', user);
+    }).catch((error) => {
+      console.log('Error - NOT saved', error);
     });
-
-
-// Github Organizations list 
-// gitServer.get('/Orgs', (req, res) => {
-//   let orgId = 0;
-//   api.listOrganizations( function(res) {
-//     let newOrgs = JSON.parse(res);
-//     let eachOrg = newOrgs.map(org => JSON.stringify((org.login)));
-//    eachOrg.forEach(function(element) {
-//       db.Organizations.create({
-//         orgId: orgId, 
-//         orgName: element,
-//       }).then((data) => {
-//         console.log('Success - Organizations have been saved', data);
-//       }).catch((error) => {
-//         console.log('Error', error);
-//       })
-//     })
-//   })
-//   res.end();
-// });
-
-  // Github Organization by top Repos by Stargazers
-gitServer.get('/starred/orgs', (req, res) => {
-  let orgId = 0; 
-  let orgName = '';
-  api.reposByStars( function(res) {
-    let newOrg = JSON.parse(res);
-    let orgArray = newOrg.items;
-    let orgData = orgArray.map((org) => {
-       if ( org.owner.type === 'Organization') {
-       return org.owner.login;
-      }
-      return null;
-    })
-    orgData.forEach(function(element) {
-      db.Organizations.create({
-          orgId: orgId, 
-          orgName: element
-        }).then((data) => {
-          console.log('Success - Organizations have been saved', data);
-        }).catch((error) => {
-          console.log('Error', error);
-        })
-    })
-  }).catch(error => console.log('Error', error));
+  })
+    .catch(error => console.log('Error - Repo comments were NOT saved', error));
+  res.end();
 });
 
 
-// Github top Repos by stargazers 
-gitServer.get('/starred/repos', (req, res) => {
-  let repoId = 0; 
-  api.reposByStars( function(res) {
-    let newRes = JSON.parse(res);
-    let newRepo = newRes.items
-    let repoData = newRepo.map((repo) => {
-      return repo.url
-    })
-    console.log(repoData);
-      repoData.forEach(function(url) {
-        api.listComments(url, function(res) {
-          let data = JSON.parse(res);
-          let newData = data.map((repo) => {
-            if (repo.author_association !== 'CONTRIBUTOR') {
-              return repo.body;
-            }
-          })
-          newData.forEach(function(element) {
-            db.Repo.create({
-              repoId: repoId,
-              orgId: orgId, 
-              commentsBody: element,
-            }).then((data) => {
-              console.log('Success - Top Repo comments have been saved', data);
+// User's repos and User's review comments
+gitServer.get('/user/repo/review', (res) => {
+  const username = 'therobinkim';
+  const repo = 'lets-recreate-axios';
+  const repoIdNum = 0;
+  api.listCommentsInARepo(username, repo, (unit) => {
+    const newRepo = JSON.parse(unit);
+    const data = newRepo.map((repos) => {
+      if (repos.author_association !== 'CONTRIBUTOR') {
+        return repos.body;
+      }
+    });
+    db.Repo.create({
+      repoId: repoIdNum,
+      repoName: repo,
+      commentsBody: data,
+    }).then((repoData) => {
+      console.log('Repo review comments have been saved!', repoData);
+    }).catch((error) => {
+      console.log('Error - Repo Review was NOT saved', error);
+    });
+  })
+    .catch(error => console.log('Error - Repo comments were NOT saved', error));
+  res.end();
+});
+
+
+// Github Organization by top Repos by Stargazers
+gitServer.get('/starred/orgs', () => {
+  const orgId = 0;
+  api.reposByStars((unit) => {
+    const newOrg = JSON.parse(unit);
+    const orgArray = newOrg.items;
+
+    const orgData = orgArray.map((org) => {
+      if (org.owner.type === 'Organization') {
+        db.Organization.create({
+          orgId,
+          orgName: org.owner.login,
+          orgStargazers: org.stargazers_count,
+        }).then((orgs) => {
+          console.log('Success - Organizations have been saved', orgs);
+        }).catch((error) => {
+          console.log('Error', error);
+        });
+      }
+    });
+  });
+});
+
+
+// Github top Repos by stargazers
+gitServer.get('/starred/repos', () => {
+  const topId = 0;
+  api.reposByStars((res) => {
+    const newRes = JSON.parse(res);
+    const newRepo = newRes.items;
+    const repoData = newRepo.map((repo) => {
+      db.TopRepo.create({
+        topId,
+        topRepoName: repo.name,
+        topRepoStargazers: repo.stargazers_count,
+        updatedAt: repo.updatedAt,
+      });
+      return repo.url;
+    });
+    repoData.forEach((url) => {
+      api.listComments(url, (unit) => {
+        const data = JSON.parse(unit);
+        const newData = data.map((repo) => {
+          if (repo.author_association !== 'CONTRIBUTOR') {
+            db.TopRepo.update({
+              commentsBody: repo.body,
+            }, {
+              where: {
+                topId,
+              },
+            }).then((resData) => {
+              console.log('Success - Top Repo comments have been saved', resData);
             }).catch((error) => {
               console.log('Error', error);
-            })
-          })
-        })
-      })
-  }).catch(error => console.log('Error', error));
+            });
+          }
+        });
+      });
+    });
+  });
+});
+
+// Incoming Request for ALL Organizations
+db.Organization.findAll({}).then((data) => {
+  console.log(data);
+}).catch((err) => {
+  console.log(err);
 });
