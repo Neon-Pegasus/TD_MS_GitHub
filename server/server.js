ef9fc6b972a31f8b085b8d534bcacbf3e5d7d1fd
JBen13 <jessicabenjaminxiii@gmail.com>
2018-10-11 19:07:47 -0700

    Updates code for git hub requests

--- server/server.js
+++ server/server.js
@@ -104,7 +104,7 @@ gitServer.get('/api/gateway/github/orgdata', (req, res) => {
 
 // User's repos from GitHub
 gitServer.get('/user/repos', (req, res) => {
-  const username = req.body.username || 'fabpot';
+  const username = req.body.username || 'bebraw';
   const userId = 0;
   api.getReposByUser(username, (unit) => {
     const newData = JSON.parse(unit);
@@ -122,35 +122,63 @@ gitServer.get('/user/repos', (req, res) => {
   });
 });
 
+// Get list of Users Repos
+gitServer.get('/api/gateway/github/userrepo', (req, res) => {
+  const userName = req.params.userName || 'fabpot';
+  db.User.find({ where: { userName } }).then((data) => {
+    res.send(data);
+    console.log('success', data);
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
 // User's repos and User's review comments
 gitServer.get('/user/repo/review', (req, res) => {
-  const repoIdNum = 0;
-  const username = req.body.username || 'fabpot';
-  db.User.findOne({
-    where: { userName: username },
-  }).then((data) => {
-    data.repoNameList.forEach((repo) => {
-      api.listCommentsInARepo(data.userName, repo, (unit) => {
+  const userName = 'bebraw';
+  // const array = [];
+  db.User.find({ where: { userName } }).then((data) => {
+    let dataList = data.repoNameList;
+    dataList = dataList.map((item) => {
+      return item.replace(/[\"]/gim,'');
+    });
+    dataList.forEach((repo) => {
+      api.listCommentsInARepo(userName, repo, (unit) => {
         const newUnit = JSON.parse(unit);
-        const data = newUnit.map((repos) => {
-          if (repos.author_association !== 'CONTRIBUTOR') {
-            return db.Repo.create({
-              repoId: repoIdNum,
-              // repoName: repo,
-              commentsBody: repos.body,
-              updatedAt: repos.updatedAt,
-            }).then((repoData) => {
-              res.send(repoData);
-              console.log('Repo review comments have been saved!', repoData);
-            }).catch((error) => {
-              console.log('Error - Repo Review was NOT saved', error);
-            });
+        newUnit.map((repos) => {
+          if (repos.author_association === 'OWNER') {
+            if (!Array.isArray(repos.body))  {
+              let array = [repos.body];
+              db.Repo.create({
+                repoName: repo,
+                commentsBody: array,
+                userName: userName,
+                updatedAt: repos.updatedAt,
+              }).then((repoData) => {
+                // res.send(repoData);
+                console.log('Repo review comments have been saved!', repoData);
+              }).catch((error) => {
+                console.log('Error - Repo Review was NOT saved', error);
+              });
+            } else {
+              db.Repo.create({
+                repoName: repo,
+                commentsBody: repos.body,
+                updatedAt: repos.updatedAt,
+              }).then((repoData) => {
+                // res.send(repoData);
+                console.log('Repo review comments have been saved!', repoData);
+              }).catch((error) => {
+                console.log('Error - Repo Review was NOT saved', error);
+              });
+            } 
           }
         });
       });
     });
   });
 });
+
 
 // Request for User's repos
 
