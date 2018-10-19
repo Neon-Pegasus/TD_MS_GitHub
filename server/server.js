fd1365f5599b42aaf34f67ca43b4d7a3d9907a35
JBen13 <jessicabenjaminxiii@gmail.com>
2018-10-18 21:30:53 -0700

    Fixes bug in request for OrgRepos

--- server/server.js
+++ server/server.js
@@ -15,249 +15,157 @@ gitServer.listen(port, () => {
 });
 
 
-// Get Organizations by top Repos by Stargazers
-const getOrgs = () => {
-  const array = [];
-  const arr = [];
-  let newArray = [];
-  const comments = [];
-  api.reposByStars().then((data) => {
-    const orgArray = data.items;
-    for (let i = 0; i < orgArray.length; i += 1) {
-      if (orgArray[i]) {
-        const org = orgArray[i];
-        if (org.owner.type === 'Organization') {
-          const orgObj = {
-            orgName: org.owner.login,
-            orgDescription: org.description,
-            orgAvatar: org.owner.avatar_url,
-            orgStargazers: org.stargazers_count,
-            orgRepo: org.name,
-            orgCommentsBody: '',
+const getOrgRepoData = () => {
+  let topReposList;
+  let repoComments;
+  return api.reposByStars()
+    .then((results) => {
+      topReposList = results.items.filter(repo => repo.owner.type === 'Organization')
+        .map((repo) => {
+          return {
+            orgName: repo.owner.login,
+            orgAvatar: repo.owner.avatar_url,
+            orgTopRepoUrl: repo.url,
+            orgTopRepoName: repo.name,
+            orgTopRepoStars: repo.stargazers_count,
           };
-          array.push(orgObj);
-        }
-      }
-    }
-    newArray = array.slice(0, 100);
-  })
-    .then(() => {
-      newArray.forEach((data) => {
-        arr.push(api.listOrgComments(data.orgName, data.orgRepo));
+        });
+
+      return Promise.all(topReposList.map(repo => api.getRepoComments(repo.orgTopRepoUrl)));
+    })
+    .then((results) => {
+      repoComments = results.map((orgRepo) => {
+        return orgRepo.filter((repo) => {
+          return repo.author_association === 'MEMBER';
+        });
+      });
+      const orgRepoArray = repoComments.map((org) => {
+        return org.reduce((acc, cv) => {
+          acc.organization = cv.url.split('https://api.github.com/repos/')[1].split('/')[0];
+          acc.comment = acc.comment.concat(cv.body);
+          return acc;
+        }, { comment: [] });
+      });
+      return orgRepoArray.filter((commentLine) => {
+        return commentLine.comment.length;
       });
     })
-    .then(() => Promise.all(arr))
-    // .then(result => result.filter(item.author_association === 'MEMBER').map(body => body.body);
-    //   });
-    // })
-    .then((data) => { console.log(data); })
-    .then(() => {
-      db.Organization.bulkCreate(newArray);
+    .then((results) => { 
+      results.forEach(repo => topReposList.forEach((org) => {
+        if (repo.organization === org.orgName) {
+          org.orgTopRepoComments = repo.comment;
+        }
+      }));
+      db.Organization.bulkCreate(topReposList);
+      return topReposList;
     })
-    .then((result) => { console.log(result); console.log('HERE--->', newArray); })
-    .catch((error) => { console.log(error); console.log(error.message); });
+    .then((result) => { return result; })
+    .catch((error) => { console.log(error); });
 };
 
-// const queryDatabase = (userName) => {
-//   db.User.find({ where: { userName } })
-//     .then((data) => {
-//       let dataList = data.repoNameList;
-//       dataList = dataList.map(item => item.replace(/[\"]/gim, ''));
-//       return dataList;
-//       })
-//       .then((dataList) => {
-//         dataList.forEach((repo) => {
-//           api.listCommentsInARepo(userName, repo)
-//         })
-//           .then((unit) => {
-//             const repoData = unit.map((repos) => {
-//               if (repos.author_association === 'OWNER') {
-//                 if (!Array.isArray(repos.body)) {
-//                   const array = [repos.body];
-//                   db.Repo.create({
-//                     repoName: repo,
-//                     commentsBody: array,
-//                     userName,
-//                     updatedAt: repos.updatedAt,
-//                   }).then((reposData) => {
-//                     console.log('Repo review comments have been saved!', reposData);
-//                   }).catch((error) => {
-//                     console.log('Error - Repo Review was NOT saved', error);
-//                   });
-//                 } else {
-//                   db.Repo.create({
-//                     repoName: repo,
-//                     commentsBody: repos.body,
-//                   })
-//                     .then((result) => {
-//                       console.log('Repo review comments have been saved!', result);
-//                     })
-//                     .catch((error) => {
-//                       console.log('Error - Repo Review was NOT saved', error);
-//                     });
-//                 }
-//               }
-//             });
-//           });
-//       });
-//     });
-// };
-
-// const queryDatabase = (userName) => {
-//   const promises = [];
-//   return db.User.find({ where: { userName } })
-//     .then((data) => {
-//       console.log('LINE 117', data);
-//       let dataList = data.repoNameList;
-//       // ['reponame1', 'reponame1']
-//       // dataList = dataList.map(item => item.replace(/[\"]/gim, ''));
-//       return dataList;
-//     })
-//     .then((dataList) => {
-//       dataList.forEach((repo) => { 
-//         promises.push(api.listCommentsInARepo(userName, repo));
-//       });
-//       return Promise.all(promises);
-//     })
-//     .then((data) => {
-//       return data.filter((repo) => {
-//         return repo.length;
-//       });
-//     })
-// .then((data) => {
-//   return data.map((repos) => {
-//     if (repos.author_association === 'OWNER') {
-//       if (!Array.isArray(repos.body)) {
-//         const array = [repos.body];
-//         db.Repo.create({
-//           repoName: repo,
-//           commentsBody: array,
-//           userName,
-//           updatedAt: repos.updatedAt,
-//         }).then((reposData) => {
-//           console.log('Repo review comments have been saved!', reposData);
-//           return reposData;
-//         }).catch((error) => {
-//           console.log('Error - Repo Review was NOT saved', error);
-//           return error;
-//         });
-//       } else {
-//         db.Repo.create({
-//           repoName: repo,
-//           commentsBody: repos.body,
-//         })
-//           .then((result) => {
-//             console.log('Repo review comments have been saved!', result);
-//             return result;
-//           })
-//           .catch((error) => {
-//             console.log('Error - Repo Review was NOT saved', error);
-//             return error;
-//           });
-//       }
-//     }
-// s
-// };
 
 const getUserData = (userName) => {
+  const promises = [];
   api.getReposByUser(userName)
     .then((results) => {
-      const userRepos = results.map(repo => (repo.name));
-      return db.User.create({
-        userName,
-        repoNameList: userRepos,
+      const userRepos = results.map(repo => (repo.url));
+      userRepos.forEach((url) => { 
+        promises.push(api.getRepoComments(url));
+      });
+      return Promise.all(promises);
+    })
+    .then((results) => {
+      return results.filter((repo) => {
+        return repo.length;
       });
     })
-    .then(() => { queryDatabase(userName); })
-    .catch(error => error);
-};
-
-
-// User's repos from GitHub
-const getUserRepos = (userName) => {
-  db.User.find({ where: { userName } })
-    .then((data) => {
-      if (!data) {
-        return api.getUserData(userName);
-      }
-      if (data.dataValues.userName) {
-        return queryDatabase(userName);
-      }
+    .then((results) => {
+      const userRepoArray = results.map((repos) => {
+        repos.filter(repo => repo.author_association === 'OWNER')
+        if (!Array.isArray(repos.body)) {
+          const array = [repos.body];
+          db.Repo.create({
+            userName,
+            repoName: (repo.url).split('https://api.github.com/repos/')[1].split('/')[1],
+            commentsBody: array,
+          });
+        } else {
+          db.Repo.create({
+            userName,
+            repoName: (repo.url).split('https://api.github.com/repos/')[1].split('/')[1],
+            commentsBody: repos.body,
+          });
+        }
+      });
+      return userRepoArray;
     })
-    .then((data) => { res.send(data); console.log(data); })
-    .catch((error) => { console.log(error.message); });
-};
-
-const getRepoData = (userName) => {
-  db.Repo.find({ where: { userName } })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((error) => {
-      res.send(error.message);
-    });
+    .catch((error) => { console.log('Error', error)});
 };
 
 
 /** **** API GATEWAY ********* */
+
+
 // Incoming Request for list of Organizations
 gitServer.get('/api/gateway/github/orglist', (req, res) => {
-  db.Organization.findAll({ attributes: ['orgName'] }).then((data) => {
-    res.send(data);
-    console.log('success', data);
-  }).catch((err) => {
-    console.log(err);
-  });
+  db.Organization.findAll({ attributes: ['orgName'] })
+    .then((result) => {
+      res.send(result);
+      console.log('success', result);
+    }).catch((err) => {
+      console.log(err);
+    });
 });
 
 
 // Incoming Request for Organization Data
 gitServer.get('/api/gateway/github/org/data', (req, res) => {
   const org = req.params.orgsName || 'facebook';
-  db.Organization.findOne({ where: { orgName: org } }).then((data) => {
-    res.send(data);
-    console.log(data);
-  }).catch((err) => {
-    console.log(err);
-  });
+  db.Organization.findAll({ where: { orgName: org } })
+    .then((result) => {
+      res.send(result);
+      console.log(result);
+    }).catch((err) => {
+      console.log(err);
+    });
+});
+
+gitServer.get('/api/gateway/github/org/data', (req, res) => {
+  const org = req.params.orgsName || 'facebook';
+  db.Organization.findOne({ where: { orgName: org } })
+    .then((result) => {
+      res.send(result);
+      console.log(result);
+    }).catch((err) => {
+      console.log(err);
+    });
 });
 
 
 gitServer.get('/api/gateway/github/orgdata', (req, res) => {
-  db.Organization.findOne({}).then((data) => {
-    res.send(data);
-    console.log(data);
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-
-// Get list of Users Repos
-gitServer.get('/api/gateway/github/user', (req, res) => {
-  const userName = req.params.userName || 'andrew';
-  db.User.find({ where: { userName } }).then((data) => {
-    res.send(data);
-    console.log('success', data);
-  }).catch((err) => {
-    console.log(err);
-  });
+  db.Organization.findOne({})
+    .then((result) => {
+      res.send(result);
+      console.log(result);
+    }).catch((err) => {
+      console.log(err);
+    });
 });
 
 
 // Request for specific user's repo data and comments
 gitServer.get('/api/gateway/github/user/repo/data', (req, res) => {
-  const userName = req.params.userName || 'andrew';
-  db.User.find({ where: { userName } })
-    .then((data) => {
-      if (!data) {
-        return api.getUserData(userName);
+  const userName = req.params.userName || 'fabpot';
+  db.Repo.findOne({ where: { userName } })
+    .then((result) => {
+      if (!result || result === undefined) {
+        return getUserData(userName);
       }
-      if (data.dataValues.userName) {
-        return queryDatabase(userName);
+      if (result.dataValues.userName) {
+        return db.Repo.findAll({ where: { userName } });
       }
     })
-    .then((data) => { res.send(data); console.log(data); })
+    .then((result) => { res.send(result); console.log(result); })
     .catch((error) => { res.send(error.message); });
 });
 
@@ -265,16 +173,16 @@ gitServer.get('/api/gateway/github/user/repo/data', (req, res) => {
 gitServer.get('/api/gateway/github/repo/data', (req, res) => {
   const userName = req.params.userName || 'andrew';
   db.Repo.findAll({ where: { userName } })
-    .then((data) => {
-      res.send(data);
-      console.log(data);
+    .then((result) => {
+      res.send(result);
+      console.log(result);
     }).catch((err) => {
       res.send(err.message);
     });
 });
 
 const lateNightUpdate = () => {
-  getOrgs();
+  getOrgRepoData();
   api.updateUserData();
 };
 
@@ -285,6 +193,14 @@ setInterval(lateNightUpdate, 86400000);
 // Test for deployment
 gitServer.get('/', (req, res) => {
   // api.updateUserData();
-  getOrgs();
+  // getOrgRepoData()
+  //   .then((result) => {
+  //     res.send(result);
+  //   });
+  // getOrgs();
+  // getTopOrgs()
+  // .then((result) => {
+  //   res.send(result);
+  // });
   res.send('Test - HOME PAGE!!!');
 });
